49ba62c42f49c92a0a0f56926f67a4d3ac4241fc
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-12 00:02:37 +0100

    title-needSupportSlide
    ready

--- src/components/HomePage/SecondSlider/SecondSlider.jsx
+++ src/components/HomePage/SecondSlider/SecondSlider.jsx
@@ -9,46 +9,80 @@ import { nanoid } from "nanoid";
 import Container from "@/components/container/Container";
 import Icon from "@/components/Icon/Icon";
 import css from "./SecondSlider.module.css";
+import TitleNeedSupport from "./TitleNeedSupport/TitleNeedSupport";
 
 const slides = [
-  { img: "/img/home/slider2/fire.png", id: nanoid(), title: "Збір від Оксани", subtitle: "окопні свічки", progress: "7%" },
-  { img: "/img/home/slider2/cars.png", id: nanoid(), title: "Збір від Андрія", subtitle: "автівки для зсу", progress: "54%" },
-  { img: "/img/home/slider2/items.png", id: nanoid(), title: "Збір від Майора Чорнобаєва", subtitle: "3D-друковані ортези", progress: "32%" },
-  { img: "/img/home/slider2/boxes.png", id: nanoid(), title: "Збір від Життя Переможе", subtitle: "бандажі для зсу", progress: "68%" },
+  {
+    img: "/img/home/slider2/fire.png",
+    id: nanoid(),
+    title: "Збір від Оксани",
+    subtitle: "окопні свічки",
+    progress: "7%",
+  },
+  {
+    img: "/img/home/slider2/cars.png",
+    id: nanoid(),
+    title: "Збір від Андрія",
+    subtitle: "автівки для зсу",
+    progress: "54%",
+  },
+  {
+    img: "/img/home/slider2/items.png",
+    id: nanoid(),
+    title: "Збір від Майора Чорнобаєва",
+    subtitle: "3D-друковані ортези",
+    progress: "32%",
+  },
+  {
+    img: "/img/home/slider2/boxes.png",
+    id: nanoid(),
+    title: "Збір від Життя Переможе",
+    subtitle: "бандажі для зсу",
+    progress: "68%",
+  },
 ];
 
 const SecondSlider = () => {
   const navigationPrevRef = useRef(null);
   const navigationNextRef = useRef(null);
   return (
-    <Container>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        loop={true}
-        modules={[Navigation]}
-        navigation={{
-          prevEl: navigationPrevRef.current,
-          nextEl: navigationNextRef.current,
-        }}
-        onBeforeInit={(swiper) => {
-          swiper.params.navigation.prevEl = navigationPrevRef.current;
-          swiper.params.navigation.nextEl = navigationNextRef.current;
-        }}
-        className={css.mySwiper}
-      >
-        {slides.map(({ img, id, title, subtitle, progress }) => (
-          <SwiperSlide key={id}>
-            <p className={css.titleSlide}>{title}</p>
-            <p className={css.subtitleSlide}>{subtitle}</p>
-            <div className={css.progressBar}><span style={{width: `${progress}`}}>{progress}</span></div>
-            <Image alt="photo" src={img} width={228} height={157} priority />
-          </SwiperSlide>
-        ))}
-        <div ref={navigationPrevRef} className={css.arrowLeftBtn}><Icon name={'icon-arrow-left'} className={css.arrowLeftIcon}/></div>
-        <div ref={navigationNextRef} className={css.arrowRightBtn}><Icon name={'icon-arrow-right'} className={css.arrowRightIcon}/></div>
-      </Swiper>
-    </Container>
+    <>
+      <Container>
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          loop={true}
+          modules={[Navigation]}
+          navigation={{
+            prevEl: navigationPrevRef.current,
+            nextEl: navigationNextRef.current,
+          }}
+          onBeforeInit={(swiper) => {
+            swiper.params.navigation.prevEl = navigationPrevRef.current;
+            swiper.params.navigation.nextEl = navigationNextRef.current;
+          }}
+          className={css.mySwiper}
+        >
+          {slides.map(({ img, id, title, subtitle, progress }) => (
+            <SwiperSlide key={id}>
+              <p className={css.titleSlide}>{title}</p>
+              <p className={css.subtitleSlide}>{subtitle}</p>
+              <div className={css.progressBar}>
+                <span style={{ width: `${progress}` }}>{progress}</span>
+              </div>
+              <Image alt="photo" src={img} width={228} height={157} priority />
+            </SwiperSlide>
+          ))}
+          <div ref={navigationPrevRef} className={css.arrowLeftBtn}>
+            <Icon name={"icon-arrow-left"} className={css.arrowLeftIcon} />
+          </div>
+          <div ref={navigationNextRef} className={css.arrowRightBtn}>
+            <Icon name={"icon-arrow-right"} className={css.arrowRightIcon} />
+          </div>
+        </Swiper>
+      </Container>
+      <TitleNeedSupport />
+    </>
   );
 };
 
---
+++ src/components/HomePage/SecondSlider/TitleNeedSupport/TitleNeedSupport.jsx
@@ -0,0 +1,29 @@
+import Container from "@/components/container/Container";
+import Icon from "@/components/Icon/Icon";
+import css from "./TitleNeedSupport.module.css";
+
+const TitleNeedSupport = () => {
+  return (
+    <div className={css.titleContainer}>
+      <Container>
+        <div className={css.mainTitleContainer}>
+          <h2 className={css.mainTitle}>
+            потребуємо вашої
+            <span className={css.titleAccent}>підтримки</span>
+          </h2>
+          <p className={css.mainTitleText}>
+            Зробіть пожертву на виготовлення (3D-друк) необхідних принад для
+            українських захисників. Кожен внесок має значення, адже ви
+            підтримуєте наших героїв та зміцнюєте оборону нашої держави.
+          </p>
+          <button type="button" className={css.buttonSupport}>
+          <span className={css.btnText}><Icon className={css.iconSupportBtn} name={"icon-banknote"} />
+          підтримати</span>
+        </button>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default TitleNeedSupport;
