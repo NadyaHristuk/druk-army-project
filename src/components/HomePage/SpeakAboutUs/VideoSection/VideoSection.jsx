f12a012ef8174d6483b28c5df0b9512974d7d242
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-13 12:58:36 +0100

    add-video

--- src/components/HomePage/SpeakAboutUs/VideoSection/VideoSection.jsx
+++ src/components/HomePage/SpeakAboutUs/VideoSection/VideoSection.jsx
@@ -13,6 +13,30 @@ const VideoSection = () => {
           className={css.speakAboutImage}
         />
       </div>
+      <div className={css.videoContainer}>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/PUv4p9GvwFU?si=gppEaBZNVzNHTJjA&amp;start=13"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+          className={css.video}
+        ></iframe>
+      </div>
+      <div className={css.videoContainer}>
+        <iframe
+          width="560"
+          height="315"
+          className={css.video}
+          src="https://www.youtube.com/embed/VBEy43hjCRA?si=aj3gU9cBAsjo1pc-&amp;start=7"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 };
