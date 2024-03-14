126b300c57adfe9c1c9fe35540a516edf438a4c4
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-14 13:45:42 +0100

    video modal
    create modal for video

--- src/components/HomePage/SpeakAboutUs/VideoSection/VideoSection.jsx
+++ src/components/HomePage/SpeakAboutUs/VideoSection/VideoSection.jsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
+import Modal from "../Modal/Modal";
 import css from "./VideoSection.module.css";
 
 const VideoSection = () => {
+  const [openFirstModal, setOpenFirstModal] = useState(false);
+  const [openSecondModal, setOpenSecondModal] = useState(false);
+
+  const operateScroll = () => {
+    if (typeof window !== "undefined") {
+      if(openFirstModal || openSecondModal) {
+        window.document.body.style.overflow = 'hidden';
+      } else {
+        window.document.body.style.overflow = 'auto';
+      }
+    }
+  }
+  operateScroll();
+
   return (
     <div className={css.videoSection}>
       <div className={css.imageContainer}>
@@ -14,7 +32,11 @@ const VideoSection = () => {
         />
       </div>
       <div className={css.videoContainer}>
-        <button type="button" className={css.btnModalYoutube}>
+        <button
+          type="button"
+          className={css.btnModalYoutube}
+          onClick={() => setOpenFirstModal(true)}
+        >
           <Image
             src={"/img/home/video-youtube/youtube3.webp"}
             alt="Новини, де про нас говорять"
@@ -23,7 +45,10 @@ const VideoSection = () => {
           />
         </button>
       </div>
-      <div className={css.videoContainer}>
+      <div
+        className={css.videoContainer}
+        onClick={() => setOpenSecondModal(true)}
+      >
         <button type="button" className={css.btnModalYoutube}>
           <Image
             src={"/img/home/video-youtube/youtube2.webp"}
@@ -33,6 +58,34 @@ const VideoSection = () => {
           />
         </button>
       </div>
+      {openFirstModal && (
+        <Modal setOpenFirstModal={setOpenFirstModal}>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/PUv4p9GvwFU?si=xnk8RIf9OPdS5ud_&amp;start=23"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            className={css.video}
+          ></iframe>
+        </Modal>
+      )}
+      {openSecondModal && (
+        <Modal setOpenSecondModal={setOpenSecondModal}>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VBEy43hjCRA?si=FvR5afv_GeBBB5dh"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            className={css.video}
+          ></iframe>
+        </Modal>
+      )}
     </div>
   );
 };
