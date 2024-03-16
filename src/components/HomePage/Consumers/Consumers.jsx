9ec7bd27b892f8ef50a8a1e7d498b7087fe152ee
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-16 20:28:36 +0100

    update

--- src/components/HomePage/Consumers/Consumers.jsx
+++ src/components/HomePage/Consumers/Consumers.jsx
@@ -1,10 +1,12 @@
 import Container from "@/components/container/Container";
 import TitleConsumers from "./TitleConsumers/TitleConsumers";
+import ListConsumers from "./ListConsumers/ListConsumers";
 
 const Consumers = () => {
   return (
       <Container>
         <TitleConsumers />
+        <ListConsumers/>
       </Container>
   );
 };
---
+++ src/components/HomePage/Consumers/ListConsumers/ListConsumers.jsx
@@ -0,0 +1,102 @@
+import css from "./ListConsumers.module.css";
+
+const consumers = [
+  {
+    img: "/public/img/home/consumers/mark1.webp",
+    webUrl: "https://www.zsu.gov.ua/",
+  },
+  {
+    img: "/public/img/home/consumers/mark2.webp",
+    webUrl: "https://t.me/ebat_vebalo",
+  },
+  {
+    img: "/public/img/home/consumers/mark3.webp",
+    webUrl: "https://t.me/OGHB_108",
+  },
+  {
+    img: "/public/img/home/consumers/mark4.webp",
+    webUrl: "https://t.me/robert_magyar",
+  },
+  {
+    img: "/public/img/home/consumers/mark5.webp",
+    webUrl: "https://www.facebook.com/110separatebrigade",
+  },
+  {
+    img: "/public/img/home/consumers/mark6.webp",
+    webUrl: "https://t.me/kraken_kha",
+  },
+  {
+    img: "/public/img/home/consumers/mark7.webp",
+    webUrl: "https://t.me/ZSU_21obspopbr",
+  },
+  {
+    img: "/public/img/home/consumers/mark8.webp",
+    webUrl: "https://www.facebook.com/viiskovachastina3066/",
+  },
+  {
+    img: "/public/img/home/consumers/mark9.webp",
+    webUrl: "https://www.facebook.com/people/108-%D0%BE%D0%BA%D1%80%D0%B5%D0%BC%D0%B0-%D0%B1%D1%80%D0%B8%D0%B3%D0%B0%D0%B4%D0%B0-%D0%A1%D0%B8%D0%BB-%D1%82%D0%B5%D1%80%D0%B8%D1%82%D0%BE%D1%80%D1%96%D0%B0%D0%BB%D1%8C%D0%BD%D0%BE%D1%97-%D0%BE%D0%B1%D0%BE%D1%80%D0%BE%D0%BD%D0%B8-%D0%97%D0%A1%D0%A3/100083010146725/",
+  },
+  {
+    img: "/public/img/home/consumers/mark10.webp",
+    webUrl: "https://aerorozvidka.ngo/",
+  },
+  {
+    img: "/public/img/home/consumers/mark11.webp",
+    webUrl: "https://t.me/horticka_varta",
+  },
+  {
+    img: "/public/img/home/consumers/mark12.webp",
+    webUrl: "https://www.instagram.com/59_ompbr/",
+  },
+  {
+    img: "/public/img/home/consumers/mark13.webp",
+    webUrl: "https://www.facebook.com/people/%D0%A0%D0%90%D0%A0%D0%9E%D0%93-%D1%83%D0%B4%D0%B0%D1%80%D0%BD%D1%96-%D0%B4%D1%80%D0%BE%D0%BD%D0%B8-24-%D0%9E%D0%9C%D0%91%D1%80/100091477793844/",
+  },
+  {
+    img: "/public/img/home/consumers/mark14.webp",
+    webUrl: "https://sof.mil.gov.ua/",
+  },
+  {
+    img: "/public/img/home/consumers/mark15.webp",
+    webUrl: "https://t.me/ZSU_21obspopbr",
+  },
+  {
+    img: "/public/img/home/consumers/mark16.webp",
+    webUrl: "https://t.me/PEGAS_1OTBr",
+  },
+  {
+    img: "/public/img/home/consumers/mark17.webp",
+    webUrl: "https://t.me/jahznebes",
+  }
+];
+
+const consumersBlurs = [
+    {
+        blurImg: "/public/img/home/consumers/blurMark1.webp"
+    },
+    {
+        blurImg: "/public/img/home/consumers/blurMark2.webp"
+    },
+    {
+        blurImg: "/public/img/home/consumers/blurMark3.webp"
+    },
+    {
+        blurImg: "/public/img/home/consumers/blurMark1.webp"
+    },
+    {
+        blurImg: "/public/img/home/consumers/blurMark2.webp"
+    },
+    {
+        blurImg: "/public/img/home/consumers/blurMark3.webp"
+    },
+    {
+        blurImg: "/public/img/home/consumers/blurMark1.webp"
+    }
+]
+
+const ListConsumers = () => {
+  return <div>ListConsumers</div>;
+};
+
+export default ListConsumers;
