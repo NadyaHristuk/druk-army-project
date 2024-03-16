c4f56a54a93676bfa3ecafe07a464d1aa5d9034f
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-16 08:35:44 +0100

    update

---
+++ src/components/HomePage/Slider_News/SliderNews.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import economistImg from "/public/img/home/slider3/economist-soldier.webp";
+import economistImgTwo from "/public/img/home/slider3/economist.webp";
+import dwImg from "/public/img/home/slider3/dw-press.webp";
+import drukImg from "/public/img/home/slider3/druk.webp";
+import css from "./SliderNews.module.css";
+
+const SliderNews = () => {
+  return (
+    <div>SliderNews</div>
+  )
+}
+
+export default SliderNews
