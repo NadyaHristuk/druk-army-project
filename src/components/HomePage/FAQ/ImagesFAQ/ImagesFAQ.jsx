4574fe601bbbb4f8f1f6427d592ebe13af377c8a
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-17 16:19:08 +0100

    FAQ 768px
    ready

--- src/components/HomePage/FAQ/ImagesFAQ/ImagesFAQ.jsx
+++ src/components/HomePage/FAQ/ImagesFAQ/ImagesFAQ.jsx
@@ -11,6 +11,8 @@ import css from "./ImagesFAQ.module.css";
 const ImagesFAQ = ({setRole, role}) => {
   const media480 = useMediaQuery({minWidth: 480});
   const media767 = useMediaQuery({maxWidth: 767.98});
+  const media768 = useMediaQuery({minWidth: 768});
+  const media959 = useMediaQuery({maxWidth: 959.98});
   
 const handleClick = (e) => {
   let roleChoosen = e.currentTarget.id;
@@ -24,8 +26,8 @@ const handleClick = (e) => {
           <Image
             src={soldierImg}
             alt="український військовий"
-            width={media480 && media767 ? 210.61 : 148.87}
-            height={media480 && media767 ? 156.79 : 110.82}
+            width={media480 && media767 ? 210.61 : (media768 && media959 ? 172.44 : 148.87)}
+            height={media480 && media767 ? 156.79 : (media768 && media959 ? 128.37 : 110.82)}
             className={role === "soldier" ? css.cardImgActive : css.cardImg}
           />
           <div className={role === "soldier" ? css.textContainerActive : css.textContainer}>
@@ -36,8 +38,8 @@ const handleClick = (e) => {
           <Image
             src={donorImg}
             alt="спонсор"
-            width={media480 && media767 ? 205.35 : 145.14}
-            height={media480 && media767 ? 179.61 : 126.95}
+            width={media480 && media767 ? 205.35 : (media768 && media959 ? 168.13 : 145.14)}
+            height={media480 && media767 ? 179.61 : (media768 && media959 ? 147.05 : 126.95)}
             className={role === "donor" ? css.cardImgActive : css.cardImg}
           />
           <div  className={role === "donor" ? css.textContainerActive : css.textContainer}>
@@ -48,8 +50,8 @@ const handleClick = (e) => {
           <Image
             src={printerImg}
             alt="друкар, що виробляє 3D-вироби"
-            width={media480 && media767 ? 195.4 : 138.11}
-            height={media480 && media767 ? 187.21 : 132.32}
+            width={media480 && media767 ? 195.4 : (media768 && media959 ? 159.98 : 138.11)}
+            height={media480 && media767 ? 187.21 : (media768 && media959 ? 153.28 : 132.32)}
             className={role === "printer" ? css.cardImgActive : css.cardImg}
           />
           <div  className={role === "printer" ? css.textContainerActive : css.textContainer}>
@@ -60,8 +62,8 @@ const handleClick = (e) => {
           <Image
             src={creatorImg}
             alt="виробник"
-            width={media480 && media767 ? 196.57 : 138.94}
-            height={media480 && media767 ? 188.38 : 133.15}
+            width={media480 && media767 ? 196.57 : (media768 && media959 ? 160.94 : 138.94)}
+            height={media480 && media767 ? 188.38 : (media768 && media959 ? 154.24 : 133.15)}
             className={role === "producer" ? css.cardImgActive : css.cardImg}
           />
           <div className={role === "producer" ? css.textContainerActive : css.textContainer}>
