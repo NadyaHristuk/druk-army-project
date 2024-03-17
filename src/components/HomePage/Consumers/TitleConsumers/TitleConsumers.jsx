cf9647243058ed8bb02a70b678d90217fe6515d4
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-17 22:50:52 +0100

    update

--- src/components/HomePage/Consumers/TitleConsumers/TitleConsumers.jsx
+++ src/components/HomePage/Consumers/TitleConsumers/TitleConsumers.jsx
@@ -4,14 +4,12 @@ import { useTranslation } from "react-i18next";
 import css from "./TitleConsumers.module.css";
 
 const TitleConsumers = () => {
-const { t } = useTranslation();
+  const { t } = useTranslation();
 
   return (
     <div className={css.mainTitleContainer}>
       <h2 className={css.mainTitle}>{t("home:consumers")}</h2>
-      <p className={css.mainTitleText}>
-        {t("home:consumersInfo")}
-      </p>
+      <p className={css.mainTitleText}>{t("home:consumersInfo")}</p>
     </div>
   );
 };
