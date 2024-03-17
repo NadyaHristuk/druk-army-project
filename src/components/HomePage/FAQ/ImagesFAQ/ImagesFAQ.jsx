7407b7f79c05a5581a726dd914440f0e81d0aa42
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-17 17:27:41 +0100

    FAQ 1200px
    ready

--- src/components/HomePage/FAQ/ImagesFAQ/ImagesFAQ.jsx
+++ src/components/HomePage/FAQ/ImagesFAQ/ImagesFAQ.jsx
@@ -15,6 +15,8 @@ const ImagesFAQ = ({setRole, role}) => {
   const media959 = useMediaQuery({maxWidth: 959.98});
   const media960 = useMediaQuery({minWidth: 960});
   const media1199 = useMediaQuery({maxWidth: 1199.98});
+  const media1200 = useMediaQuery({minWidth: 1200});
+  const media1919 = useMediaQuery({maxWidth: 1919.98});
   
 const handleClick = (e) => {
   let roleChoosen = e.currentTarget.id;
@@ -28,8 +30,8 @@ const handleClick = (e) => {
           <Image
             src={soldierImg}
             alt="український військовий"
-            width={media480 && media767 ? 210.61 : (media768 && media959 ? 172.44 : (media960 && media1199 ? 288.99 : 148.87))}
-            height={media480 && media767 ? 156.79 : (media768 && media959 ? 128.37 : (media960 && media1199 ? 215.13 : 110.82))}
+            width={media480 && media767 ? 210.61 : (media768 && media959 ? 172.44 : (media960 && media1199 ? 288.99 : (media1200 && media1919 ? 272.16 : 148.87)))}
+            height={media480 && media767 ? 156.79 : (media768 && media959 ? 128.37 : (media960 && media1199 ? 215.13 : (media1200 && media1919 ? 202.61 : 110.82)))}
             className={role === "soldier" ? css.cardImgActive : css.cardImg}
           />
           <div className={role === "soldier" ? css.textContainerActive : css.textContainer}>
@@ -40,8 +42,8 @@ const handleClick = (e) => {
           <Image
             src={donorImg}
             alt="спонсор"
-            width={media480 && media767 ? 205.35 : (media768 && media959 ? 168.13 : (media960 && media1199 ? 281.76 : 145.14))}
-            height={media480 && media767 ? 179.61 : (media768 && media959 ? 147.05 : (media960 && media1199 ? 246.44 : 126.95))}
+            width={media480 && media767 ? 205.35 : (media768 && media959 ? 168.13 : (media960 && media1199 ? 281.76 : (media1200 && media1919 ? 265.36 : 145.14)))}
+            height={media480 && media767 ? 179.61 : (media768 && media959 ? 147.05 : (media960 && media1199 ? 246.44 : (media1200 && media1919 ? 232.09 : 126.95)))}
             className={role === "donor" ? css.cardImgActive : css.cardImg}
           />
           <div  className={role === "donor" ? css.textContainerActive : css.textContainer}>
@@ -52,8 +54,8 @@ const handleClick = (e) => {
           <Image
             src={printerImg}
             alt="друкар, що виробляє 3D-вироби"
-            width={media480 && media767 ? 195.4 : (media768 && media959 ? 159.98 : (media960 && media1199 ? 268.11 : 138.11))}
-            height={media480 && media767 ? 187.21 : (media768 && media959 ? 153.28 : (media960 && media1199 ? 256.88 : 132.32))}
+            width={media480 && media767 ? 195.4 : (media768 && media959 ? 159.98 : (media960 && media1199 ? 268.11 : (media1200 && media1919 ? 252.5 : 138.11)))}
+            height={media480 && media767 ? 187.21 : (media768 && media959 ? 153.28 : (media960 && media1199 ? 256.88 : (media1200 && media1919 ? 241.92 : 132.32)))}
             className={role === "printer" ? css.cardImgActive : css.cardImg}
           />
           <div  className={role === "printer" ? css.textContainerActive : css.textContainer}>
@@ -64,8 +66,8 @@ const handleClick = (e) => {
           <Image
             src={creatorImg}
             alt="виробник"
-            width={media480 && media767 ? 196.57 : (media768 && media959 ? 160.94 : (media960 && media1199 ? 269.72 : 138.94))}
-            height={media480 && media767 ? 188.38 : (media768 && media959 ? 154.24 : (media960 && media1199 ? 258.48 : 133.15))}
+            width={media480 && media767 ? 196.57 : (media768 && media959 ? 160.94 : (media960 && media1199 ? 269.72 : (media1200 && media1919 ? 254.02 : 138.94)))}
+            height={media480 && media767 ? 188.38 : (media768 && media959 ? 154.24 : (media960 && media1199 ? 258.48 : (media1200 && media1919 ? 243.43 : 133.15)))}
             className={role === "producer" ? css.cardImgActive : css.cardImg}
           />
           <div className={role === "producer" ? css.textContainerActive : css.textContainer}>
