7650195a3791253b0b00a6a02a9849286fe10dd4
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-19 10:32:02 +0100

    Loader translation

--- src/components/PreLoader/PreLoader.jsx
+++ src/components/PreLoader/PreLoader.jsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import css from "@/components/PreLoader/PreLoader.module.css";
 
 const PreLoader = () => {
   const [progress, setProgress] = useState(0);
+  const { t } = useTranslation();
   useEffect(() => {
     const interval = setInterval(() => {
       if (progress < 100) {
@@ -19,13 +21,13 @@ const PreLoader = () => {
     <div className={css.loaderContainer}>
       <div className={css.loaderContent}>
         <div className={css.upperText}>
-          <p className={css.textLoading}>Завантаження</p>
+          <p className={css.textLoading}>{t("header:loading")}</p>
           <span className={css.progressNumber}>{progress}%</span>
         </div>
         <div className={css.progressBar}>
           <div className={css.progress} style={{ width: `${progress}%` }}></div>
         </div>
-        <p className={css.textEnd}>Завантажуємо фуру с файлами css js</p>
+        <p className={css.textEnd}>{t("header:loadText")}</p>
       </div>
     </div>
   );
