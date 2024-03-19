b446b63cf3e5b843f8186a566f45f2c02f7d6aaf
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-03-19 10:48:34 +0100

    if Esc close settings

--- src/components/header/HeaderSettings/HeaderSettings.jsx
+++ src/components/header/HeaderSettings/HeaderSettings.jsx
@@ -3,7 +3,7 @@
 import Icon from "@/components/Icon/Icon";
 import css from "@/components/header/HeaderSettings/HeaderSettings.module.css";
 import MenuSettings from "../MenuSettings/MenuSettings";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const HeaderSettings = () => {
   const [isShowSettings, setIsShowSettings] = useState(false);
@@ -11,6 +11,18 @@ const HeaderSettings = () => {
   const handleClickSettings = () => {
     setIsShowSettings((prev) => !prev);
   };
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.key === 'Escape') {
+          isShowSettings && setIsShowSettings(false);
+      }
+    };
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, [isShowSettings]);
 
   return (
     <div className={css.settings}>
